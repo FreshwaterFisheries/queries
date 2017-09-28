91dbaf04c876c80da656ad3424097f7369d4b54e
Bonnie Robert <bonnie.robert@gofishbc.com>
2017-09-28 14:28:36 -0700

    Add files via upload

---
+++ clear_views.sql
@@ -0,0 +1,12 @@
+DROP TABLE ffsbc.view_1_Lakes
+DROP TABLE ffsbc.view_1a_Lake_Profiles
+DROP TABLE ffsbc.view_1b_Lake_Access
+DROP TABLE ffsbc.view_1c_Lake_Dimensions
+DROP TABLE ffsbc.view_2_Assessment_Summary
+DROP TABLE ffsbc.view_2a_Net_Summary
+DROP TABLE ffsbc.view_2b_Biological_Data
+DROP TABLE ffsbc.view_2c_Net_Summary_Counts
+DROP TABLE ffsbc.view_4_Creel
+DROP TABLE ffsbc.view_4a_Creel_Counts
+DROP TABLE ffsbc.view_5_Effort
+DROP TABLE ffsbc.view_7_Project
